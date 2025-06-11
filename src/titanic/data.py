2376ced7114a5911cfbb8484c7328d9abd2b9694
Clement Mercier <clementjrmercier@gmail.com>
2025-06-11 13:45:55 +0000

    data Load, preprocess, prepare

--- src/titanic/data.py
+++ src/titanic/data.py
@@ -5,6 +5,7 @@ Load, preprocess, prepare, and save the Titanic dataset.
 import pandas as pd
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import OneHotEncoder
 import os
 
 def load_data(file_name: str):
@@ -51,7 +52,16 @@ def prepare_data(df:pd.DataFrame) -> tuple[pd.DataFrame, pd.Series]:
         tuple: A tuple containing [X,y] the features DataFrame and the target Series.
     """
     numeric_features = ['Age', 'Fare']
+    categorical_features =  ["Sex", "Embarked"]
 
     scaler = StandardScaler()
+    encoder = OneHotEncoder(sparse_output=False, handle_unknown='ignore', drop='first').set_output(transform="pandas")
+
     df_scaled = df.copy()
+
     df_scaled[numeric_features] = scaler.fit_transform(df[numeric_features])
+    df_encoded = encoder.fit_transform(df[categorical_features])
+
+    df_final = pd.concat([df_scaled, df_encoded], axis=1).drop(columns=['Sex', 'Embarked'])
+
+    return (df_final.drop(columns=["Survived"]), df_final["Survived"])
